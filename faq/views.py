8b1f8c837c522e735bc2ce6f1a7aaffced7a6a76
Kushal Goyal <goyalkushal04@gmail.com>
2025-02-01 09:46:33 +0000

    fix: Add caching

--- faq/views.py
+++ faq/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework_extensions.cache.mixins import CacheResponseMixin
 from .models import FAQ
 from .serializers import FAQSerializer
 
-class FAQViewSet(viewsets.ModelViewSet):
+class FAQViewSet(CacheResponseMixin, viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
+    cache_key = 'faqs_cache'
