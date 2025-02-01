c9391b6129b05d40ffc4b044edf7bbf2567f35fb
Kushal Goyal <goyalkushal04@gmail.com>
2025-02-01 10:09:18 +0000

    fix: cache bug fix

--- faq/views.py
+++ faq/views.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets
-from rest_framework_extensions.cache.mixins import CacheResponseMixin
 from .models import FAQ
 from .serializers import FAQSerializer
 
-class FAQViewSet(CacheResponseMixin, viewsets.ModelViewSet):
+class FAQViewSet(viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
-    cache_key = 'faqs_cache'
+    
